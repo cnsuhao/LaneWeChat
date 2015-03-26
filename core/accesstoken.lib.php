02e8465efdad87e6db04571bc9a51d22a30defaa
lane <lane@zhouyuanjiangxp.corp.qihoo.net>
2015-03-26 23:58:04 +0800

    BUG、开源协议、文档

--- core/accesstoken.lib.php
+++ core/accesstoken.lib.php
@@ -10,9 +10,6 @@ namespace LaneWeChat\Core;
  * Website: http://www.lanecn.com
  */
 class AccessToken{
-    public static function test(){
-        echo 'hello world';
-    }
     /**
      * 获取微信Access_Token
      */
@@ -29,7 +26,7 @@ class AccessToken{
      * @descrpition 从微信服务器获取微信ACCESS_TOKEN
      * @return Ambigous|bool
      */
-    private function _getAccessToken(){
+    private static function _getAccessToken(){
         $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.WECHAT_APPID.'&secret='.WECHAT_APPSECRET;
         $accessToken = Curl::callWebServer($url, '', 'GET');
         if(!isset($accessToken['access_token'])){
